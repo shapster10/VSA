53c654c6b3f7a9962619dbfd8dbd2090b3640c4d
shapster10 <40013807+shapster10@users.noreply.github.com>
2018-06-12 13:45:26 -0500

    Add files via upload

---
+++ thing.py
@@ -0,0 +1,71 @@
+
+name = raw_input("What is your name? ")
+
+
+for x in range(1,201):
+    y = raw_input('Enter somethin')
+    if y == 'hi':
+        print 'bye'
+    elif y== 'Charlie' or y == 'charlie':
+        print 'is ded meme'
+    elif y == 'Brad' or y == 'brad':
+        print 'is better than charlie'
+    elif y== 'why':
+        print 'because'
+    elif y== 'Are you my friend':
+         print 'no'
+    elif y == 'nothing':
+         print 'what'
+    elif y== 'yes':
+        print 'no bc you bad'
+    elif y== 'stupid':
+        print 'dumb kid'
+    elif y== "i'm sad":
+        print "don't worry"
+    elif y == "I'm sad":
+        print "don't worry"
+    elif y == "im sad":
+        print "don't worry"
+    elif y == "Im sad":
+        print "don't worry"
+    elif y == 'no':
+        print 'yes'
+    elif y == 'what' or y == 'What':
+        print 'ur dumb'
+    elif y == 'hey' or y == 'Hey':
+        print 'sup'
+    elif y == 'random':
+        print 'ask charlie he is the one making the game'
+    elif y == 'ai' or y == 'Ai':
+        print 'yep?'
+    elif y == 'is charlie a good programmer':
+        print "Yes, but a ded ded meme"
+    elif y == 'cool':
+        print "i know i am"
+    elif y == "wow":
+        print 'wow is right'
+    elif y== "operate" or y == "Operate":
+        print "operate on what"
+    elif y == "me" or y == 'Me':
+        print "but u bad"
+    elif y == 'you' or y == 'You':
+        print 'yeah im better than you'
+    elif y == 'bye' or y == 'Bye':
+        print 'see ya'
+    elif y == 'kill' or y == 'Kill':
+        print 'yourself'
+    elif y == 'kill urself' or y == 'kill yourself':
+        print 'yeah you should'
+    elif y == 'rude' or y == 'you are rude':
+        print 'and so what'
+    elif y == 'i hate you':
+        print 'same'
+    elif y ==
+    else:
+        print 'so what'
+# #
+# #
+# y = 1000
+# while y == 1000:
+#     x = raw_input("Enter a word")
+#     print "ai says " + str(x)
