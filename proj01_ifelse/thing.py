0c2b06d1c8c9a535668d7147c4c8618508dea50c
shapster10 <40013807+shapster10@users.noreply.github.com>
2018-06-12 09:52:31 -0500

    Add files via upload

---
+++ proj01_ifelse/thing.py
@@ -0,0 +1,22 @@
+for x in range(1,41):
+    y = raw_input('Enter Something')
+    if y == 'hi':
+        print 'bye'
+    elif y== 'Charlie':
+        print 'is ded meme'
+    elif y == 'Brad':
+        print 'is better than charlie'
+    elif y== 'why':
+        print 'because'
+    elif y== 'no':
+        print 'yes'
+    elif y== 'hey':
+        print 'sup'
+    elif y== 'are you my friend':
+        print 'yes'
+    elif y== 'Are you my friend':
+        print 'no'
+    elif y== 'yes':
+        print 'no bc you bad'
+    else:
+        print 'your dumb'
