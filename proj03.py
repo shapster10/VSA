53e49b66d8ffeae1127e043dcee9954d4b6eba8c
shapster10 <40013807+shapster10@users.noreply.github.com>
2018-06-13 10:42:35 -0500

    Add files via upload

--- proj03.py
+++ proj03.py
@@ -11,5 +11,53 @@ or exactly right. Keep the game going until the user types exit.
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 
 """
+game = 1
 
+play = raw_input("Lets play a guessing game, press enter to play or type 'EXIT' to end game.")
+if play != "EXIT":
+    limit = int(raw_input('Choose a guess limit:'))
+    if limit == 1:
+        limit = limit + 1
+        print "Set to minimum of 2"
+    elif limit < 1:
+        print "Set to minimum of 2"
+        limit = 2
+    elif limit > 30:
+        print "Set limit to maximum of 30"
+    x = int(raw_input("Choose a bottom number:"))
+    y = int(raw_input("Choose a top number:"))
+    if x > y:
+        print "Set numbers to default of 1 and 100"
+        x = 1
+        y = 100
+    elif x == y:
+        print "Set numbers to default of 1 and 100"
+        x = 1
+        y = 100
+    print "I am thinking of a number between " + str(x) + " and " + str(y) + "."
+    import random
+    rnum = random.randint(x, y)
+
+    while game == 1 and limit > 0:
+        limit = int(limit) - 1
+        guess = raw_input("Guess a number or enter 'EXIT' to exit the game:")
+        if guess == "Exit" or guess == "exit" or guess == "EXIT" or guess == "EXit" or guess == "EXIt" or guess == "eXit" or guess == "eXIt" or guess == "eXIT":
+            game = 0
+        else:
+            if limit == 0:
+                print 'You ran out of guesses. Game over. The number was. ' + str(rnum)
+            else:
+                if int(guess) > y:
+                    print "That is not an option."
+                elif int(guess) < x:
+                    print "That is not an option."
+                elif int(guess) > rnum:
+                    print 'Your answer is too high!'
+                elif int(guess) < rnum:
+                    print 'Your answer is too low!'
+                else:
+                        print "Congragulations you got the number."
+                        game = 0
+else:
+    print "Goodbye."
 
