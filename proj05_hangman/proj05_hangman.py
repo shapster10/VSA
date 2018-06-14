cadb3af41db8745a53dba94a46a401e4fd81fbbc
shapster10 <40013807+shapster10@users.noreply.github.com>
2018-06-14 14:55:57 -0500

    Add files via upload

--- proj05_hangman/proj05_hangman.py
+++ proj05_hangman/proj05_hangman.py
@@ -29,6 +29,95 @@ def load_words():
     print "  ", len(wordlist), "words loaded."
     return wordlist
 
+
+#full
+a = '   |----'
+c = '   |---O'
+d = '   |  -|-'
+e = "   |  / \\"
+f = '   |'
+g = '___|_______'
+
+
+
+
+
+c1 = '   |---'#empty
+d1 = '   |  '#empty
+e1 = "   |  "#empty
+
+
+e2 = "   |  / "#one leg
+
+
+
+d2 = '   |   |'#body
+d3 = '   |  -|'#one arm
+
+# print a
+# print b
+# print c
+# print d1
+# print e1
+# print f
+# print g
+
+
+def stage(something):
+    if something == '0':
+        print c1
+        print d1
+        print e1
+        print f
+        print g
+    elif something == '1':
+        print a
+        print c
+        print d1
+        print e1
+        print f
+        print g
+    elif something == '2':
+        print a
+        print c
+        print d2
+        print e1
+        print f
+        print g
+    elif something == '3':
+        print a
+        print c
+        print d3
+        print e1
+        print f
+        print g
+    elif something == '4':
+        print a
+        print c
+        print d
+        print e1
+        print f
+        print g
+    elif something == '5':
+        print a
+        print c
+        print d
+        print e2
+        print f
+        print g
+    elif something == '6':
+        print a
+        print c
+        print d
+        print e
+        print f
+        print g
+print
+
+
+
+
+
 def choose_word(wordlist):
     """
     wordlist (list): list of words (strings)
@@ -50,6 +139,7 @@ word = choose_word(wordlist)
 str.lower(word)
 guesses = 6
 print 'Welcome to hangman! I am thinking of a word with ' + str(len(word)) + " letters. You have six guesses."
+stage('0')
 x = 1
 y = 2
 z = 0
@@ -92,10 +182,11 @@ while x == 1 and guesses > 0:
         print 'Wrong guess.'
         guesses = int(guesses) - 1
         print str(int(guesses)) + ' guesses left.'
+        stage(str(6 - guesses))
         if guesses == 0:
             print 'YOU LOSE! NO MORE GUESSES ARE AVAILABLE.'
             print "The word was " + word + "."
     if '_ ' not in underscore:
-        print 'CONGRAGULATIONS! YOU WON THE HARDEST GAME YOULL EVER PLAY.'
+        print "CONGRAGULATIONS! YOU WON THE HARDEST GAME YOU'LL EVER PLAY."
         print "You guessed the word " + word + "."
         x = 0
